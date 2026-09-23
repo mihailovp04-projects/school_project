479dba4d4fb1ae8d75c2f0a74f6a26b802ce0682
mihailovp04 <mihailovp04@gmail.com>
2026-09-23 22:49:24 +0300

    Add StudentRepository for Data Access Layer

---
+++ School.Data/Repositories/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using School.Domain;
+
+namespace School.Data.Repositories;
+
+public interface IStudentRepository
+{
+    Task<List<Student>> GetAllAsync();
+    Task<Student?> GetByIdAsync(int id);
+    Task AddAsync(Student student);
+    Task UpdateAsync(Student student);
+    Task DeleteAsync(int id);
+}
---
+++ School.Data/Repositories/StudentRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School.Domain;
+
+namespace School.Data.Repositories;
+
+public class StudentRepository : IStudentRepository
+{
+    private readonly SchoolDbContext _context;
+
+    public StudentRepository(SchoolDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Student>> GetAllAsync()
+    {
+        return await _context.Students
+            .Include(s => s.SchoolClass)
+            .ToListAsync();
+    }
+
+    public async Task<Student?> GetByIdAsync(int id)
+    {
+        return await _context.Students
+            .Include(s => s.SchoolClass)
+            .FirstOrDefaultAsync(s => s.Id == id);
+    }
+
+    public async Task AddAsync(Student student)
+    {
+        _context.Students.Add(student);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Student student)
+    {
+        _context.Students.Update(student);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        if (student != null)
+        {
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
